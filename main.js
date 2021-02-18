d8169cc84b0b849847adb90035551f6ed79d238e
Lynne16 <76992610+Lynne16@users.noreply.github.com>
2021-02-18 20:41:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ function add_item(){
     var item=document.getElementById("add_item").value;
     menu_list_array.push(item);
     menu_list_array.sort();
-    htmldata="<section class='cards>'"
+    htmldata="<section class='cards'>"
     for(var i =0;i<menu_list_array.length; i++){
-        htmldata=htmldata + "<div class='cards'>" + "<img src='images/pizzaImg.png'/>" +menu_list_array[i] + "</div>" 
+        htmldata=htmldata + "<div class='card'>" + "<img src='images/pizzaImg.png'/>" +menu_list_array[i] + "</div>" 
     }
     htmldata=htmldata+"</section>"
     document.getElementById("display_addedmenu").innerHTML=htmldata;
